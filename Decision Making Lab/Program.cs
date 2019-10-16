5171380f10fb656c412954ee1fd2521e1649e869
chrishasbeard <chrisholmes16@gmail.com>
2019-10-15 22:00:20 -0400

    the program loops but doesnt end

--- Decision Making Lab/Program.cs
+++ Decision Making Lab/Program.cs
@@ -6,16 +6,27 @@ namespace Decision_Making_Lab
     {
         static void Main(string[] args)
         {
+            string userName = string.Empty;
+
             Console.WriteLine("Hello, what is your name?");
-            string userName = Console.ReadLine();
-            Console.Write("Hello " + userName + ", please enter a number that is between 1 & 100");
-            double userNumber = Convert.ToDouble(Console.ReadLine());
+            userName = Console.ReadLine();
+            bool doLoop = false;
 
             do
             {
 
+                Console.Write("Hello " + userName + ", please enter a number that is between 1 & 100: ");
+                double userNumber = Convert.ToDouble(Console.ReadLine());
 
-                if (userNumber % 2 == 0)
+
+                if (userNumber < 1 || userNumber > 100)
+                {
+                    Console.WriteLine("please enter a number 1 throught 100");
+                }
+
+
+
+                else if (userNumber % 2 == 0)
                 {
                     if (userNumber < 25 && userNumber >= 2)
                     {
@@ -29,24 +40,30 @@ namespace Decision_Making_Lab
                     {
                         Console.WriteLine(userNumber + " and even.");
                     }
+                }
 
-                    else if (userNumber % 2 != 0)
+                else if (userNumber % 2 != 0)
+                {
+                    if (userNumber > 60)
                     {
-                        if (userNumber > 60)
-                        {
-                            Console.WriteLine("Odd and greater than 60.");
-                        }
-                        else if (userNumber < 60)
-                        {
-                            Console.WriteLine(userNumber + " and odd.");
-                        }
+                        Console.WriteLine("Odd and greater than 60.");
+                    }
+                    else if (userNumber < 60)
+                    {
+                        Console.WriteLine(userNumber + " and odd.");
                     }
                 }
-            } while ();
-            
-        }
-    }
-}
+
+                else if (userNumber < 1 || userNumber > 100)
+                {
+                    doLoop = false;
+                }
+
+            } while (doLoop == false);
+          }   
+     }
+ }
+
 
 
 
